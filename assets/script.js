dd5edfb537554c1fa6c5915dce28c2bf2514e7ee
Maeva95 <maeva.tarati@gmail.com>
2023-06-20 10:08:10 +0200

    positionnement des flèches et création dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,76 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+console.log(slides[1]["image"])
+
+// ajout des flèches de navigation du slider
+let arrowPrev = document.querySelector(".arrow_left");
+let arrowNext = document.querySelector(".arrow_right");
+
+// ajout des Event Listener des flèches
+arrowPrev.addEventListener("click", () => {
+    console.log("retour!");
+    prevSlide();
+})
+arrowNext.addEventListener("click", function() {
+    console.log("avance!");
+    nextSlide()
+})
+// fin de la partie flèches de navigation
+
+// ajout des bullet points
+
+for (let i = 0; i < slides.length; i++) {
+    let dotContainer = document.querySelector(".dots");
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dotContainer.appendChild(dot);
+}
+
+//fin ajout des bullet points
+
+// création fonction changement slides next et prev
+
+let i=0;
+let imageSlide = document.querySelector(".banner-img");
+imageSlide.src = slides[0]["image"];
+let textSlide = banner.querySelector("p");
+let dotContainer = document.querySelector(".dots");
+dot = dotContainer.querySelectorAll(".dot")
+
+function nextSlide() {
+    i++;
+    if (i > slides.length-1) {
+        i=0;
+    }
+    if (i<0){
+        i = slides.length-1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        dot[i].className = dot[i].className.replace(" dot_selected", "");
+    }
+    imageSlide.src = slides[i]["image"];
+    imageSlide.alt = slides[i]["tagLine"];
+    textSlide.innerHTML = slides[i]["tagLine"];
+    dot[i].className += " dot_selected";
+}
+function prevSlide() {
+    i--;
+    if (i<0){
+        i=slides.length-1;
+    }
+    if (i > slides.length-1) {
+        i=0;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        dot[i].className = dot[i].className.replace(" dot_selected", "");
+    }
+
+    dot[i].className += " dot_selected";
+    imageSlide.src = slides[i]["image"];
+    imageSlide.alt = slides[i]["tagLine"];
+    textSlide.innerHTML = slides[i]["tagLine"];
+}
