f566666a3fb092e94e0018610f08e05dc9558fac
Maeva95 <maeva.tarati@gmail.com>
2023-06-28 12:09:08 +0200

    integration de la fonction refreshSlide

--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,100 @@
 const slides = [
 	{
-		"image":"./assets/images/slideshow/slide1.jpg",
+		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide2.jpg",
+		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide3.jpg",
+		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide4.png",
+		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 
-console.log(slides[0]["image"])
+// Sélection des éléments du DOM
+const imageSlide = document.querySelector(".banner-img");
+const textSlide = banner.querySelector("p");
+const dotContainer = document.querySelector(".dots");
 
-// ajout des flèches de navigation du slider
-let arrowPrev = document.querySelector(".arrow_left");
-let arrowNext = document.querySelector(".arrow_right");
 
-// ajout des Event Listener des flèches
-arrowPrev.addEventListener("click", () => {
-    console.log("retour!");
-    prevSlide();
-})
-arrowNext.addEventListener("click", function() {
-    console.log("avance!");
-    nextSlide()
-})
-// fin de la partie flèches de navigation
+// ajout des flèches de navigation du slider:
+const arrowPrev = document.querySelector(".arrow_left");
+const arrowNext = document.querySelector(".arrow_right");
 
-// ajout des bullet points
+let position = 0;
+
+
+// Ajout des Event Listener sur les flèches:
+
+arrowPrev.addEventListener("click", prevSlide)
+arrowNext.addEventListener("click", nextSlide)
+
+
+// Création des bullet points du slider:
 
 for (let i = 0; i < slides.length; i++) {
-    let dotContainer = document.querySelector(".dots");
     const dot = document.createElement("div");
     dot.classList.add("dot");
     dotContainer.appendChild(dot);
 }
 
-//fin ajout des bullet points
+const dots = dotContainer.querySelectorAll(".dot");
+// ajout du dot_selected sur le 1er bullet point
+
+
+function refreshDots() {
+    dots.forEach(dot => {
+        dot.classList.remove("dot_selected");
+    })
+	dots[position].classList.add("dot_selected");
+
+}
+
+refreshDots()
+refreshSlide()
+
 
 // création fonction changement slides next et prev
 
-let i=0;
-let imageSlide = document.querySelector(".banner-img");
-imageSlide.src = slides[0]["image"];
-let textSlide = banner.querySelector("p");
-let dotContainer = document.querySelector(".dots");
-dot = dotContainer.querySelectorAll(".dot")
-
+    /* changement de position de la slide à la prochaine position */
 function nextSlide() {
-    i++;
-    if (i > slides.length-1) {
-        i=0;
+    
+    position++;
+    if (position > slides.length-1) {
+        position=0;
     }
-    if (i<0){
-        i = slides.length-1;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        dot[i].className = dot[i].className.replace(" dot_selected", "");
-    }
-    imageSlide.src = slides[i]["image"];
-    imageSlide.alt = slides[i]["tagLine"];
-    textSlide.innerHTML = slides[i]["tagLine"];
-    dot[i].className += " dot_selected";
+    refreshDots()
+    refreshSlide()
 }
-function prevSlide() {
-    i--;
-    if (i<0){
-        i=slides.length-1;
-    }
-    if (i > slides.length-1) {
-        i=0;
-    }
 
-    for (let i = 0; i < slides.length; i++) {
-        dot[i].className = dot[i].className.replace(" dot_selected", "");
+    /* changement de position de la slide à la position précédente */
+
+function prevSlide() {
+    
+    position--;
+    if (position<0){
+        position=slides.length-1;
     }
-    imageSlide.src = slides[i]["image"];
-    imageSlide.alt = slides[i]["tagLine"];
-    textSlide.innerHTML = slides[i]["tagLine"];
-	dot[i].className += " dot_selected";
+    refreshDots()
+    refreshSlide()
+
+}
+
+// Fonction va permettre de positionner tous les élements liés à la slide visionnée
+
+function refreshSlide (){
+    const slide = slides[position];
+    const image = slide.image;
+    const tagLine = slide.tagLine;
+    imageSlide.src = `./assets/images/slideshow/${image}`;
+    imageSlide.alt = tagLine;
+    textSlide.innerHTML = tagLine;
 
 }
